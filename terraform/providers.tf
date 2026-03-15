1b5fa5cb47d6d04b7366c2df9c085697ab53232b
Chiba-Kier <hit0bit@yahoo.co.jp>
2026-03-15 19:55:26 +0900

    initialize terraform

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# AWSプロバイダーの設定
+provider "aws" {
+  region  = "ap-northeast-1" # 東京リージョン
+  profile = "default"        # aws configureで設定するプロファイル名
+}
